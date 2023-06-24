0526ab4e17efb1f4885081e61724b04fa6b5fdac
Altamiro Silva <altamirus@hotmail.com>
2023-06-23 23:32:50 -0300

    Correcao erros reserse

--- contact/admin.py
+++ contact/admin.py
@@ -9,7 +9,7 @@ from contact import models
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
-    # list_filter = 'created_date',
+    list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
